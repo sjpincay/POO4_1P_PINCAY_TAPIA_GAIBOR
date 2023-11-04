301aaca7eb18afc3fcb87ece0c433e166fc0b2d0
sjpin <sjpin@DESKTOP-QO06MN1>
2023-11-04 00:33:18 -0500

    Prueba del commit 4

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -15,6 +15,7 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola esta es una prueba :3 ");
     }
     
 }
