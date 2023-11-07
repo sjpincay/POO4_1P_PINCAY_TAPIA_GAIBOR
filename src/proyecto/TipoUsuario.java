c5aae1e4fa8acf9b1f297152d63cb8197b7e40aa
AndresT16 <joseandres1627@gmail.com>
2023-11-06 23:31:55 -0500

    Prueba enum TipoUsusario

--- src/proyecto/TipoUsuario.java
+++ src/proyecto/TipoUsuario.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
+
 package proyecto;
 
-/**
- *
- * @author sjpin
- */
+//prueba2
 public enum TipoUsuario {
     C, R
 }
