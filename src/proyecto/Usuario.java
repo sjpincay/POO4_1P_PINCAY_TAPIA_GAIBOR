23e575134aa6c338d4b9c0241325039748f4ced8
sjpin <sjpin@DESKTOP-QO06MN1>
2023-11-06 21:15:22 -0500

    Metodos getters y setters de la clase usuario terminados

--- src/proyecto/Usuario.java
+++ src/proyecto/Usuario.java
@@ -17,10 +17,48 @@ public class Usuario {
     private TipoUsuario tipo;
     
     public Usuario(String cedula,String nombre,String apellido , String user,String contraseña,TipoUsuario tipo ){
+        this.cedula= cedula;
+        this.nombre= nombre;
         this.apellido= apellido;
+        this.user= user;
+        this.contraseña= contraseña;
+        this.tipo= tipo;
     }
-    
-    public void getCedula(){
-        
+    public String getCedula(){
+       return cedula; 
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+       return apellido; 
+    }
+    public String getUser(){
+        return user;
+    }
+    public String getContraseña(){
+        return contraseña;
+    }
+    public TipoUsuario getTipo(){
+        return tipo;
+    }
+
+    public void setCedula(){
+       this.cedula= cedula; 
+    }
+    public void setNombre(){
+        this.nombre= nombre;
+    }
+    public void setApellido(){
+       this.apellido= apellido; 
+    }
+    public void setUser(){
+        this.user= user;
+    }
+    public void setContraseña(){
+        this.contraseña= contraseña;
+    }
+    public void setTipo(){
+        this.tipo= tipo;
     }
 }
