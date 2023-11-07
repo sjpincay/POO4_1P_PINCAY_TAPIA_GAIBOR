e5b933c47827653fc11c1c176e244bfc9517ffaf
jeacgaib <jeacgaib@espol.edu.ec>
2023-11-06 22:12:50 -0500

    Agregar clase vehiculo con sus metodos getters y setters

---
+++ src/proyecto/Vehiculo.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Vehiculo {
+    private String placa;
+    private String  modelo;
+    private String  marca;
+    private TipoVehiculo  tipoVehiculo;
+    private int codigoVehiculo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public TipoVehiculo getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(TipoVehiculo tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public int getCodigoVehiculo() {
+        return codigoVehiculo;
+    }
+
+    public void setCodigoVehiculo(int codigoVehiculo) {
+        this.codigoVehiculo = codigoVehiculo;
+    }
+    
+    
+}
