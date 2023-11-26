960c7909538b0f5f17fa666ea968f1c3fd7dada0
sjpin <sjpin@DESKTOP-QO06MN1>
2023-11-25 21:12:08 -0500

    ordenando paquetes y clases

--- src/proyecto/Enums/TipoUsuario.java
+++ src/proyecto/Enums/TipoUsuario.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
-package proyecto;
+package proyecto.Enums;
 
 /**
  *
- * @author Lenovo
+ * @author sjpin
  */
-public enum TipoVehiculo {
-    A,M
+public enum TipoUsuario {
+    C, R
 }
--- src/proyecto/Enums/TipoVehiculo.java
+++ src/proyecto/Enums/TipoVehiculo.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
-package proyecto;
+package proyecto.Enums;
 
 /**
  *
  * @author sjpin
  */
-public enum TipoUsuario {
-    C, R
+public enum TipoVehiculo {
+    A,M
 }
