9566dc0622e8995bd3f88b90f8514ad850fe795b
sjpin <sjpin@DESKTOP-QO06MN1>
2023-11-26 20:17:55 -0500

    corrigiendo el sistema

--- src/Principales/Sistema.java
+++ src/Principales/Sistema.java
@@ -5,7 +5,7 @@
 package Principales;
 //import static ManejoArchivos.ManejoArchivos.LeeFichero;
 import Enum.*;
-import ManejoArchivos.ManejoArchivos;
+import static ManejoArchivos.ManejoArchivos.LeerValidar;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
@@ -15,31 +15,25 @@ import java.util.Scanner;
 public class Sistema {
 
     /**
-     * @param args the command line arguments
+     * 
      */
     static ArrayList<Usuario>listUsuarios= new ArrayList<>();
     static ArrayList<Servicio>listServicios= new ArrayList<>();
     static ArrayList<Vehiculo>listVehiculos= new ArrayList<>();
     
-    /*
-    Método que muestra el menú del conductor
-    */
+    /**
+     * Método que muestra el menú del conductor
+     */
     public static void mostrarMenuConductor(){
         System.out.println("1. Consultar Servicio Asignado\n2. Datos de su vehículo");
     }
-    /*
-    Método que muestra el menú del cliente
-    */
+    /**
+     * Método que muestra el menú del cliente
+     */
     public static void mostrarMenuCliente(){
         System.out.println("1. Solicitar servicio de taxi\n2. Solicitar comida a domicilio\n3. Solicitar entrega encomienda\n4. Consultar servicios");
     }
-    /*
-    Método que lee el archivo "usuarios.txt" y crea objetos de Usuario
-    luego de que se lean los daots, los objetos se agregan a l lista de Usuarios
-    */
-    public static void cargarUsuarios(){
-        //ArrayList<String[]> datosUsuarios = 
-    }
+    
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
