73163afe568f42d431e2f556c4c78064ba9eaecf
jeacgaib <jeacgaib@espol.edu.ec>
2023-12-02 17:39:37 -0500

    Aplicar bien el uso de los metodos de ManejoArchivo

--- src/Principales/Conductor.java
+++ src/Principales/Conductor.java
@@ -4,22 +4,19 @@
  */
 package Principales;
 import Enum.EstadoConductor;
+import Enum.TipoUsuario;
 import java.util.ArrayList;
 import java.util.Scanner;
 import Enum.TipoVehiculo;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
+import ManejoArchivos.*;
 /**
  * La clase Conductor hereda de la clase Usuario y representa a un conductor en el sistema.
  */
 public class Conductor extends Usuario{
-    private int edad;
     private String numLicencia;
     private Vehiculo vehiculo;
     private EstadoConductor estadoConductor;
-    private ArrayList<Servicio> serviciosAsignados;
+    private ArrayList<Servicio> serviciosAsignados = new ArrayList<>();
     private TipoVehiculo tipoVehiculo;
     /**
      * Constructor de la clase Conductor.
@@ -35,71 +32,65 @@ public class Conductor extends Usuario{
      * @param contraseña La contraseña del conductor.
      * @param numCelular El número de celular del conductor.
      */
-    public Conductor(String numLicencia, Vehiculo vehiculo, EstadoConductor estadoConductor, String cedula, String nombre, String apellido, int edad, String user, String contraseña, String numCelular) {
-    super(cedula, nombre, apellido, user, contraseña, numCelular);
-    this.edad=edad;
-    this.numLicencia = numLicencia;
-    this.vehiculo = vehiculo;
-    this.estadoConductor = estadoConductor;
-    this.serviciosAsignados = new ArrayList<>();  
-}
-    /**
-     * Obtiene el vehículo del conductor.
-     *
-     * @return El vehículo del conductor.
-     */
-    public Vehiculo getVehiculo() {
-        return vehiculo;
+    public Conductor(String numLicencia, Vehiculo vehiculo, EstadoConductor estadoConductor, String cedula, String celular, String nombre, String apellido, String user, String contraseña, TipoUsuario tipo) {
+        super(cedula, celular, nombre, apellido, user, contraseña, tipo);
+        this.numLicencia = numLicencia;
+        this.vehiculo = vehiculo;
+        this.estadoConductor = estadoConductor;
     }
-    /**
-     * Obtiene el número de licencia del conductor.
-     *
-     * @return El número de licencia del conductor.
-     */    
+
     public String getNumLicencia() {
         return numLicencia;
     }
-    /**
-     * Establece el número de licencia del conductor.
-     *
-     * @param numLicencia El nuevo número de licencia del conductor.
-     */
+
     public void setNumLicencia(String numLicencia) {
         this.numLicencia = numLicencia;
     }
-    /**
-     * Obtiene el estado del conductor.
-     *
-     * @return El estado del conductor.
-     */
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
     public EstadoConductor getEstadoConductor() {
         return estadoConductor;
     }
-    
-    /**
-     * Consulta los servicios asignados al conductor.
-     */   
-    @Override
-    public void consultarServicio() {
-        // mostrar los servicios asignados al conductor
-        // recorrerla e imprimir cada servicio
-        for (Servicio servicio : serviciosAsignados) {
-            System.out.println(servicio);
+
+    public void setEstadoConductor(EstadoConductor estadoConductor) {
+        this.estadoConductor = estadoConductor;
+    }
+
+    public TipoVehiculo getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(TipoVehiculo tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public ArrayList<Servicio> getServiciosAsignados() {
+        return serviciosAsignados;
+    }
+
+    public void setServiciosAsignados(ArrayList<Servicio> serviciosAsignados) {
+        this.serviciosAsignados = serviciosAsignados;
+    }
+
+    public static String tipo(String nombreArchivo, String code) {
+        ArrayList<String> lineas = ManejoArchivos.LeeFichero(nombreArchivo);
+        for (String linea : lineas) {
+            String[] datos = linea.split(",");
+            if (datos[0].equals(code)) {
+                return datos[4];
+            }
         }
+        return "";
     }
     
-    /**
-     * Muestra los datos del vehículo del conductor.
-     */
-    public void mostrarDatosVehiculo() {
-        //mostrar los datos del vehículo del conductor
-        System.out.println(vehiculo.mostrarDatos());
-    }
-    /**
-     * Permite al conductor seleccionar una opción del menú.
-     *
-     * @return La opción seleccionada por el conductor.
-     */
+    
     public int seleccionarOpcion() {
         int opcion = 0;
         Scanner sc = new Scanner(System.in);
@@ -131,58 +122,4 @@ public class Conductor extends Usuario{
                 break;
         }
     }
-     public static String tipo(String nombrearchivo, String code) {
-        String informacion = "";
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-
-        try {
-            // Apertura del fichero y creacion de BufferedReader para poder
-            // hacer una lectura comoda (disponer del metodo readLine()).
-            archivo = new File(nombrearchivo);
-            fr = new FileReader(archivo, StandardCharsets.UTF_8);
-            br = new BufferedReader(fr);
-
-            // Lectura del fichero
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split(",");
-                if (datos[0].equals(code)) {
-                    informacion = datos[4];
-
-                }
-
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // En el finally cerramos el fichero, para asegurarnos
-            // que se cierra tanto si todo va bien como si salta 
-            // una excepcion.
-            try {
-                if (null != fr) {
-                    fr.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        }
-        return informacion;
-    }    
-    /**
-     * Verifica si el conductor está disponible.
-     *
-     * @return Verdadero si el conductor está disponible, falso en caso contrario.
-     */    
-
-    }
-    /**
-     * Verifica si el vehículo del conductor es un auto o una moto.
-     *
-     * @return El tipo de vehículo del conductor.
-     */    
-    
-    
 }
