b5a78c41c8009060dc91a84b40662cea83387af8
sjpincay <sjpincay@espol.edu.ec>
2023-12-03 17:29:42 -0500

    Clase Cliente arreglada y completada

--- src/Principales/Cliente.java
+++ src/Principales/Cliente.java
@@ -4,20 +4,25 @@
  */
 package Principales;
 
-import Enum.TipoUsuario;
+import Enum.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-import ManejoArchivos.*;
+import java.util.Random;
+import java.time.LocalDate;
+import ManejoArchivos.ManejoArchivos;
 
 /**
- * La clase Cliente hereda de la clase Usuario y representa a un cliente en el sistema.
+ * La clase Cliente hereda de la clase Usuario y representa a un cliente en el
+ * sistema.
  */
-public class Cliente extends Usuario{
+public class Cliente extends Usuario {
 
+    private int edad;
     private String tarjetaCredito;
-    private ArrayList<Usuario> usuarios;
-    private ArrayList<Servicio> listaServicio = new ArrayList();
-    
+    private ArrayList<Servicio> serviciosSolicitados;
+    private ArrayList<Pagos> pagosRealizados;
+    private TipoUsuario tipoUsuario;
+
     /**
      * Constructor de la clase Cliente.
      *
@@ -30,70 +35,324 @@ public class Cliente extends Usuario{
      * @param numCelular El número de celular del cliente.
      * @param tarjetaCredito El número de la tarjeta de crédito del cliente.
      */
-    public Cliente(String tarjetaCredito, String cedula, String celular, String nombre, String apellido, String user, String contraseña, TipoUsuario tipo, int edad) {
-        super(cedula, celular, nombre, apellido, user, contraseña, tipo, edad);
+    public Cliente(String cedula, String nombre, String apellido, String user, String contraseña, String numCelular, int edad, String tarjetaCredito) {
+        super(cedula, nombre, apellido, user, contraseña, numCelular);
+        this.edad = edad;
         this.tarjetaCredito = tarjetaCredito;
+        this.serviciosSolicitados = new ArrayList<>();
+        this.pagosRealizados = new ArrayList<>();
     }
 
+    public ArrayList<Pagos> getPagosRealizados() {
+        return pagosRealizados;
+    }
+
+    public void setPagosRealizados(ArrayList<Pagos> pagosRealizados) {
+        this.pagosRealizados = pagosRealizados;
+    }
+
+    public ArrayList<Servicio> getServiciosSolicitados() {
+        return serviciosSolicitados;
+    }
+
+    public void setServiciosSolicitados(ArrayList<Servicio> serviciosSolicitados) {
+        this.serviciosSolicitados = serviciosSolicitados;
+    }
+
+    /**
+     * Obtiene la edad del cliente.
+     *
+     * @return La edad del cliente.
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Obtiene el número de la tarjeta de crédito del cliente.
+     *
+     * @return El número de la tarjeta de crédito del cliente.
+     */
     public String getTarjetaCredito() {
         return tarjetaCredito;
     }
 
+    /**
+     * Establece la edad del cliente.
+     *
+     * @param edad La nueva edad del cliente.
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Establece el número de la tarjeta de crédito del cliente.
+     *
+     * @param tarjetaCredito El nuevo número de la tarjeta de crédito del
+     * cliente.
+     */
     public void setTarjetaCredito(String tarjetaCredito) {
         this.tarjetaCredito = tarjetaCredito;
     }
-    
-    public ArrayList<Servicio> getListaServicio() {
-        return listaServicio;
-    }    
-    
-    public void setListaServicio(Servicio servicio) {
-        this.listaServicio.add(servicio);
-    }
-    
-    
+
+    /**
+     * Consulta los servicios disponibles para el cliente.
+     */
     @Override
-    public String toString() {
-        return super.toString() + "[ Edad: " + getEdad() + " N.Tarjeta: " + getTarjetaCredito() + "]";
-    }
-    
-
-    public void ConsultarServicio() {
-    Scanner sc = new Scanner(System.in);
-    String validar;
-
-    do {
-        for (Servicio s : listaServicio) {
-            if (s instanceof Taxi) {
-                Taxi taxi = (Taxi) s;
-                System.out.println("Servicio de Taxi\n" + taxi.toString() + "\n");
-            }
-            if (s instanceof Encomienda) {
-                Encomienda enco = (Encomienda) s;
-                System.out.println("Servicio de Encomienda\n" + enco.toString() + "\n");
+    public void consultarServicio() {
+        System.out.println();
+        ArrayList<Servicio> servicios = this.getServiciosSolicitados();
+        if (servicios.isEmpty()) {
+            System.out.println("NO HAY INFORMACION PARA MOSTRAR");
+            System.out.println();
+        } else {
+            for (Servicio i : servicios) {
+                i.mostrarInformacion();
+                System.out.println();
             }
         }
-        System.out.println("¿Solicitar otro Servicio? (si/no): ");
-        validar = sc.nextLine();
-    } while (validar.equalsIgnoreCase("si"));
-}
-    
-    //Lee el archivo, varifica la cedula
-    public static boolean validarcliente(String nombreArchivo, Usuario usuario) {
-        ArrayList<String> lineas = ManejoArchivos.LeeFichero(nombreArchivo);
-        for (String linea : lineas) {
-        String[] datos = linea.split(",");
-        String cedula = datos[0];
-            if (cedula.equals(usuario.getCedula())) {
-                return true;
-            }     
-        }
-        return false;
     }
-    
-    //Agrega al cliente
-    public static void registrarCliente(int edad, String numTarjeta, String cedula, String nombreArchivo) {
-    String linea = cedula + "," + edad + "," + numTarjeta;
-    ManejoArchivos.EscribirArchivo(nombreArchivo, linea);
-   }
+
+    /**
+     * Permite al cliente seleccionar una opción del menú.
+     *
+     */
+    public void seleccionarOpcion() {
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println();
+            System.out.println("+++++++MENÚ+++++++");
+            System.out.println("1. Solicitar servicio de taxi");
+            System.out.println("2. Solicitar entrega encomienda");
+            System.out.println("3. Consultar servicios");
+            System.out.println("4. Salir");
+            System.out.println("Por favor, elige una opción:");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1 -> {
+                    this.solicitarTaxi();
+                }
+                case 2 -> {
+                    this.solicitarEncomienda();
+                }
+                case 3 -> {
+                    this.consultarServicio();
+                }
+                case 4 -> {
+                    break;
+                }
+                default -> {
+                    System.out.println("Opcion no valida. Vuelva a intentar.");
+                }
+            }
+        } while (opcion != 4);
+    }
+
+    public void solicitarTaxi() {
+        //Verificamos los conductores disponibles
+        ArrayList<Conductor> conductoresDisponible = new ArrayList<>();
+        Conductor cAsig;
+        for (Usuario u : Sistema.listUsuarios) {
+            if (u.getTipoUsuario() == TipoUsuario.R) {
+                Conductor cDisponible = (Conductor) u; //<<<<--------
+                if ((cDisponible.getEstadoConductor() == EstadoConductor.D) && (cDisponible.getVehiculo().getTipoVehiculo() == TipoVehiculo.A)) {
+                    conductoresDisponible.add(cDisponible);
+                }
+            }
+        }
+
+        int numCondDisponibles = conductoresDisponible.size();
+        if (conductoresDisponible.isEmpty()) {
+            System.out.println();
+            System.out.println("NO HAY CONDUCTORES DISPONIBLES");
+            System.out.println();
+        } else {
+            Random rd = new Random();
+            int indexCond = rd.nextInt(numCondDisponibles);
+            cAsig = conductoresDisponible.get(indexCond);
+            //Pedimos los datos del servicio de taxi al usuario
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Ingrese la direccion de origen: ");
+            String origen = sc.nextLine();
+            System.out.print("Ingrese la direccion de destino: ");
+            String destino = sc.nextLine();
+            LocalDate currentDate = LocalDate.now();
+            System.out.print("Ingrese la hora del viaje(formato 00:00): ");
+            String horaString = sc.nextLine();
+            String datosHora[] = horaString.split(":");
+            int hora = Integer.parseInt(datosHora[0]);
+            int minutos = Integer.parseInt(datosHora[1]);
+            System.out.print("Indique el numero de personas: ");
+            int numPersonas = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Ingrese el metodo de pago(TC/E): ");
+            String metPago = sc.nextLine();
+            TipoPago tPago;
+            Taxi t;
+            switch (metPago) {
+                case "TC" -> {
+                    tPago = TipoPago.TC;
+                    t = new Taxi(origen, destino, currentDate, cAsig, numPersonas, hora, tPago);
+                    t.calcularValorAPagar(TipoPago.TC);
+                }
+                case "tc" -> {
+                    tPago = TipoPago.TC;
+                    t = new Taxi(origen, destino, currentDate, cAsig, numPersonas, hora, tPago);
+                    t.calcularValorAPagar(TipoPago.TC);
+                }
+                case "E" -> {
+                    tPago = TipoPago.E;
+                    t = new Taxi(origen, destino, currentDate, cAsig, numPersonas, hora, tPago);
+                    t.calcularValorAPagar();
+                }
+                case "e" -> {
+                    tPago = TipoPago.E;
+                    t = new Taxi(origen, destino, currentDate, cAsig, numPersonas, hora, tPago);
+                    t.calcularValorAPagar();
+                }
+                default -> {
+                    tPago = TipoPago.E;
+                    t = new Taxi(origen, destino, currentDate, cAsig, numPersonas, hora, tPago);
+                    t.calcularValorAPagar();
+                }
+            }
+            boolean confirmacion = t.confirmarViaje();
+            if (confirmacion) {
+                this.getServiciosSolicitados().add(t);
+                t.getConductorAsignado().getServiciosAsignados().add(t);
+                String lineaAEscribir = t.getID() + ",T," + this.getCedula() + "," + t.getConductorAsignado().getNombre() + "," + t.getOrigen() + "," + t.getDestino() + "," + t.getFecha().toString() + "," + Integer.toString(t.getHora()) + ":" + Integer.toString(minutos);
+                ManejoArchivos.EscribirArchivo("servicios.txt", lineaAEscribir);
+                this.generarPago(t, tPago);
+                t.escribirArchivoViaje();
+                t.mostrarInformacion();
+            }
+        }
+
+    }
+
+    public void solicitarEncomienda() {
+        //Verificamos los conductores disponibles
+        ArrayList<Conductor> conductoresDisponible = new ArrayList<>();
+        Conductor cAsig;
+        for (Usuario u : Sistema.listUsuarios) {
+            if (u.getTipoUsuario() == TipoUsuario.R) {
+                Conductor cDisponible = (Conductor) u;
+                if ((cDisponible.getEstadoConductor() == EstadoConductor.D) && (cDisponible.getVehiculo().getTipoVehiculo() == TipoVehiculo.M)) {
+                    conductoresDisponible.add(cDisponible);
+                }
+            }
+        }
+
+        int numCondDisponibles = conductoresDisponible.size();
+        if (conductoresDisponible.isEmpty()) {
+            System.out.println();
+            System.out.println("NO HAY CONDUCTORES DISPONIBLES");
+            System.out.println();
+        } else {
+            Random rd = new Random();
+            int indexCond = rd.nextInt(numCondDisponibles);
+            cAsig = conductoresDisponible.get(indexCond);
+            //Pedimos los datos del servicio de taxi al usuario
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Ingrese la direccion de origen: ");
+            String origen = sc.nextLine();
+            System.out.print("Ingrese la direccion de destino: ");
+            String destino = sc.nextLine();
+            System.out.print("Ingrese el tipo Medicamento/Documento/Ropa (M/D/R): ");
+            String tEncomienda = sc.nextLine();
+            TipoEncomienda TE;
+            switch (tEncomienda) {
+                case "M" -> {
+                    TE = TipoEncomienda.MEDICAMENTO;
+                }
+                case "D" -> {
+                    TE = TipoEncomienda.DOCUMENTOS;
+                }
+                case "R" -> {
+                    TE = TipoEncomienda.ROPA;
+                }
+                default -> {
+                    TE = TipoEncomienda.DOCUMENTOS;
+                }
+            }
+            LocalDate currentDate = LocalDate.now();
+            System.out.print("Ingrese la hora de la entrega(formato 00:00): ");
+            String horaString = sc.nextLine();
+            String datosHora[] = horaString.split(":");
+            int hora = Integer.parseInt(datosHora[0]);
+            int minutos = Integer.parseInt(datosHora[1]);
+            System.out.print("Ingrese la cantidad de productos: ");
+            int cantidad = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Ingrese el peso total(Kg): ");
+            double peso = sc.nextDouble();
+            sc.nextLine();
+            System.out.print("Ingrese el metodo de pago(TC/E): ");
+            String metPago = sc.nextLine();
+            TipoPago tPago;
+            Encomienda e;
+            switch (metPago) {
+                case "TC" -> {
+                    tPago = TipoPago.TC;
+                    e = new Encomienda(cantidad, peso, hora, TE, origen, destino, currentDate, cAsig);
+                    e.calcularValorAPagar(TipoPago.TC);
+                }
+                case "tc" -> {
+                    tPago = TipoPago.TC;
+                    e = new Encomienda(cantidad, peso, hora, TE, origen, destino, currentDate, cAsig);
+                    e.calcularValorAPagar(TipoPago.TC);
+                }
+                case "E" -> {
+                    tPago = TipoPago.E;
+                    e = new Encomienda(cantidad, peso, hora, TE, origen, destino, currentDate, cAsig);
+                    e.calcularValorAPagar();
+                }
+                case "e" -> {
+                    tPago = TipoPago.E;
+                    e = new Encomienda(cantidad, peso, hora, TE, origen, destino, currentDate, cAsig);
+                    e.calcularValorAPagar();
+
+                }
+                default -> {
+                    tPago = TipoPago.E;
+                    e = new Encomienda(cantidad, peso, hora, TE, origen, destino, currentDate, cAsig);
+                    e.calcularValorAPagar();
+                }
+            }
+            boolean confirmacion = e.confirmarEncomienda();
+            if (confirmacion) {
+                this.getServiciosSolicitados().add(e);
+                e.getConductorAsignado().getServiciosAsignados().add(e);
+                String lineaAEscribir = e.getID() + ",E," + this.getCedula() + "," + e.getConductorAsignado().getNombre() + "," + e.getOrigen() + "," + e.getDestino() + "," + e.getFecha().toString() + "," + Integer.toString(e.getHora()) + ":" + Integer.toString(minutos);
+                ManejoArchivos.EscribirArchivo("servicios.txt", lineaAEscribir);
+                this.generarPago(e, tPago);
+                e.escribirArchivoEncomienda();
+                e.mostrarInformacion();
+            }
+        }
+
+    }
+
+    public void generarPago(Servicio s, TipoPago t) {
+        String fechaPago = s.getFecha().toString();
+        String numServicio = s.getID();
+        String formaPago = null;
+        switch (t) {
+            case TC -> {
+                formaPago = "TC";
+            }
+            case E -> {
+                formaPago = "E";
+            }
+        }
+        String subtotal = Double.toString(s.getValorAPagar());
+        String total = Double.toString(s.getValorAPagar());
+        Pagos p = new Pagos(s, this, t, s.getValorAPagar(), s.getFecha());
+        this.getPagosRealizados().add(p);
+        String lineaEscribir = Integer.toString(p.getIdPago()) + "," + fechaPago + "," + numServicio + "," + formaPago + "," + this.getCedula() + "," + subtotal + "," + total;
+        ManejoArchivos.EscribirArchivo("pagos.txt", lineaEscribir);
+    }
 }
