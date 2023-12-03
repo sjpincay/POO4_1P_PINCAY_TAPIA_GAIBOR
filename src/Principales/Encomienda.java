a29a4b42d72a302621cfcfdd96e238151300b80f
jeacgaib <jeacgaib@espol.edu.ec>
2023-12-03 17:50:00 -0500

    Se implementa la clase Encomienda

--- src/Principales/Encomienda.java
+++ src/Principales/Encomienda.java
@@ -1,38 +1,58 @@
-
 package Principales;
-import Enum.TipoEncomienda;
-import Enum.TipoVehiculo;
+
+import Enum.*;
+import ManejoArchivos.ManejoArchivos;
+import java.time.LocalDate;
 import java.util.Scanner;
-import ManejoArchivos.*;
 
 /**
- * La clase Encomienda hereda de la clase Servicio y representa un servicio de encomienda en el sistema.
+ * La clase Encomienda hereda de la clase Servicio y representa un servicio de
+ * encomienda en el sistema.
  */
-
 public class Encomienda extends Servicio {
+
     private int cantidadProducto;
     private double peso;
+    private int hora;
     private TipoEncomienda tipoEncomienda;
+
     /**
      * Constructor de la clase Encomienda.
      *
      * @param conductorAsignado El conductor asignado a la encomienda.
      */
+    
 
-
-
-    public Encomienda( String Pinicio, String Pfin, String datePago,int cantidadProducto,TipoEncomienda tipoEncomienda) {
-        super(Pinicio, Pfin, datePago);
+    public Encomienda(int cantidadProducto, double peso, int hora, TipoEncomienda tipoEncomienda, String origen, String destino, LocalDate fecha, Conductor conductorAsignado) {
+        super(origen, destino, fecha, conductorAsignado);
         this.cantidadProducto = cantidadProducto;
+        this.peso = peso;
+        this.hora = hora;
         this.tipoEncomienda = tipoEncomienda;
     }
 
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+    
     public int getCantidadProducto() {
         return cantidadProducto;
     }
 
     public void setCantidadProducto(int cantidadProducto) {
         this.cantidadProducto = cantidadProducto;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
     }
 
     public TipoEncomienda getTipoEncomienda() {
@@ -43,79 +63,66 @@ public class Encomienda extends Servicio {
         this.tipoEncomienda = tipoEncomienda;
     }
 
+    /**
+     * Calcula el valor a pagar por la encomienda.
+     */
     @Override
-    public String toString() {
-        return super.toString() + "\nCantidad de productos: " + cantidadProducto + "\nTipo de encomienda: " + tipoEncomienda;
+    public void calcularValorAPagar() {
+        double total;
+        total = (1 * this.getCantidadProducto()) + 4.0;
+        this.setValorAPagar(total);
     }
-    
-    private static TipoEncomienda tipoEncomienda() {
-        Scanner sc = new Scanner(System.in);
-        int ingreso = 5;
-        TipoEncomienda tipo = TipoEncomienda.MEDICAMENTO;
-        while (ingreso != 0) {
-            System.out.println("Seleccione una opcion\n1.Medicamentos\n2.Documentos\nIngrese una opcion:");
-            int opcion = sc.nextInt();
-            sc.nextLine();
-            switch (opcion) {
-                case 1:
-                    ingreso = 0;
-                    return tipo = TipoEncomienda.MEDICAMENTO;
-                    
-                case 2:
-                    ingreso = 0;
-                    return tipo = TipoEncomienda.DOCUMENTOS;
-                default:
-                    System.out.println("Opcion no permitida. Intentelo de nuevo\n");
-                    ingreso = 5;
-                    break;
-            }
-        }
-        return tipo;
-    }
-        
-        
-public static void crearSvEncomienda(Cliente cliente) {
-    Scanner sc = new Scanner(System.in);
-    int validar;
-    do {
-        System.out.println("Ingrese ubicacion:");
-        String ubicacion = sc.nextLine();
-        System.out.println("Ingrese destino:");
-        String destino = sc.nextLine();
-        System.out.println("Ingrese fecha(dd/mm/aaaa):");
-        String fecha = sc.nextLine();
-        System.out.println("Ingrese hora(hh:mm):");
-        String hora = sc.nextLine();
-        TipoEncomienda tipo = tipoEncomienda();
-        System.out.println("Ingrese la cantidad:");
-        int cantidad = sc.nextInt();
-        sc.nextLine();
-        String metodo = Servicio.metodoPago();//AGREGAR METODO PAGO A SERVICIO
-        System.out.println("¿Generar el servicio? (si/no)");
-        String confirmacion = sc.nextLine();
-        String confirmado = confirmacion.toLowerCase();
-        if (confirmado.equals("si")) {
-            Servicio encomienda = new Encomienda(ubicacion,destino,fecha, cantidad, tipo);
-            String tipoVehiculo = "M";
-            String conductor = encomienda.asignarConductor(tipoVehiculo); //AGREGAR METODO ASIGNARCONDUCTOR
-            String linea = encomienda.getCodigo() + "," + cliente.getNombre() + "," + conductor + "," + encomienda.getPinicio() + "," + encomienda.getPfin() + "," + encomienda.getDatePago() + "," + hora + ","+tipo+","+cantidad+","+ metodo + "," + encomienda.getValorP();
-            ManejoArchivos.EscribirArchivo("encomiendas.txt", linea);
-            System.out.println("**=============Factura=============**\n" + encomienda.toString() + "\n\n");
-            Servicio enco = (Servicio) encomienda;
-            cliente.setListaServicio(enco);
-            System.out.println("¿Solicitar otro Servicio? (si/no): ");
-            String validacion = sc.nextLine();
-            String comprobar = validacion.toLowerCase();
-            if (comprobar.equals("si")) {
-                validar = 1;
-            } else {
-                validar = 0;
-            }
+
+    public void calcularValorAPagar(TipoPago t) {
+        if (t == TipoPago.TC) {
+            double total;
+            total = (1 * this.getCantidadProducto()) + 4.0;
+            double recargo = total * 0.15;
+            total += recargo;
+            this.setValorAPagar(total);
         } else {
-            validar = 1;
+            double total;
+            total = (1 * this.getCantidadProducto()) + 4.0;
+            this.setValorAPagar(total);
         }
-    } while (validar == 1);
-}
-   
+    }
     
+    public boolean confirmarEncomienda(){
+        Scanner entrada = new Scanner(System.in);
+        boolean confEncomienda = false;
+        String respuesta;
+        do {
+            System.out.print("¿Desea confirmar encomienda? (s/n): ");
+            respuesta = entrada.next();
+            entrada.nextLine();
+            if (respuesta.equals("s")) {
+                confEncomienda = true;
+
+            } else{
+                confEncomienda = false;
+                System.out.println("Se ha cancelado el pedido. Volviendo al menú inicial.");
+            }
+        } while (!(respuesta.equals("s")) && !(respuesta.equals("n")));
+        return confEncomienda;
+    }
+    
+    public void escribirArchivoEncomienda(){
+        String lineaAEscribir = this.getID() + "," + this.getTipoEncomienda() + "," + Integer.toString(this.getCantidadProducto()) + "," + Double.toString(this.getPeso()) + "," + Double.toString(this.getValorAPagar());
+        ManejoArchivos.EscribirArchivo("viajes.txt", lineaAEscribir);
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        System.out.println();
+        System.out.println("/**************************************/");
+        System.out.println("Tipo: Encomienda");
+        System.out.println("Tipo encomienda: " + this.getTipoEncomienda());
+        System.out.println("Cantidad: " + Integer.toString(this.getCantidadProducto()));
+        System.out.println("Fecha: " + this.getFecha());
+        System.out.println("Hora: " + Integer.toString(this.getHora()));
+        System.out.println("Desde: " + this.getOrigen());
+        System.out.println("Hasta: " + this.getDestino());
+        System.out.println("Valor a pagar: " + Double.toString(this.getValorAPagar()));
+        System.out.println();
+    }
 }
